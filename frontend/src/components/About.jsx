32fcbe6ed5b82b14e17e1218ab274574fc76f5d8
12Titanium <ishan.jha2106@gmail.com>
2024-06-10 19:45:17 +0530

    1st Commit
    commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+
+      </p>
+    </section>
+  );
+};
+
+export default About;
