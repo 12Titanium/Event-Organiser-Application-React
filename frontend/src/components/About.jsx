3de88efe15283a07b1e6c8d9c2d42395086e7984
ishan jha <ishan.jha@gocomet.com>
2024-12-21 20:24:30 +0530

    Minor change in heading.

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <h4>Events</h4>
       <h2>ABOUT</h2>
       <p>
-          Welcome to KING's Events and Weddings, your premier event planning and wedding coordination company based in New Delhi, India. With years of experience in the industry, we are passionate about creating unforgettable events that reflect your unique style and vision. Our dedicated team of professionals works tirelessly to ensure every detail is perfect, leaving you free to enjoy your special day.
+          Welcome to the KING's Events and Weddings, your premier event planning and wedding coordination company based in New Delhi, India. With years of experience in the industry, we are passionate about creating unforgettable events that reflect your unique style and vision. Our dedicated team of professionals works tirelessly to ensure every detail is perfect, leaving you free to enjoy your special day.
           At KING's Events and Weddings, our mission is to turn your dreams into reality. We strive to provide exceptional service and meticulous attention to detail, ensuring that every event we organize is memorable and exceeds your expectations. Whether it's a grand wedding, a corporate event, or an intimate celebration, we are committed to making it an extraordinary experience.
       </p>
     </section>
